805895e1cb005b9ce2213831cb0bc0afdb27738c
Manak <manak.kapoor@holmusk.com>
2017-06-17 22:12:45 -0400

    Initial Commit

---
+++ static/js/projects.js
@@ -0,0 +1,140 @@
+	var project_desc = {
+	    fruumontp: {
+	        title: 'Fruumo New Tab Page',
+	        screenshot: '/static/images/project_screenshots/fruumontp.jpg',
+	        desc: 'Fruumo New Tab Page aims to make the new tab page offered by google alot more productive by giving you more information right when you open the web browser. <br/> <br/>\
+			<b> Achievements: </b>\
+			<ul>\
+			<li>Has been tried by over 90000 users</li>\
+			<li> Has a 4.5 / 5 star rating in the chrome webstore </li>\
+			<li>Has been reviewed by hundreds of Tech Weblogs</li>\
+			</ul>\
+			<b>Reviews:</b>\
+			<ul>\
+			<li> http://lifehacker.com/fruumo-is-a-feature-packed-but-fast-new-tab-page-for-ch-1440657112</li>\
+			<li> http://www.makeuseof.com/tag/fruumo-is-the-minimalistic-new-tab-page-youve-been-looking-for/</li>\
+			<li> http://www.technorms.com/25439/chrome-tabs-awesome-fruumo</li>\
+			</ul>'
+	    },
+	    fruumodm: {
+	        title: 'Fruumo Download Manager',
+	        screenshot: '/static/images/project_screenshots/fruumodm.jpg',
+	        desc: 'Fruumo Download Manager gives you a much cleaner User Interface to manage your downloads, and has many features to help you manage your downloads efficiently and automatically. It also include a multiple link downloader. <br/> <br/>\
+			<b> Achievements: </b>\
+			<ul>\
+			<li>Has an active userbase of over 25000 users</li>\
+			<li>Has 4 out of 5 star rating in the chrome webstore</li>\
+			<li>Has 300-1500 new installations everyday</li>\
+			<li>Reached 2000 users within the first 3 days of release</li>\
+			<li>Reviewed by tons of tech weblogs</li>\
+			</ul>\
+			<b>Reviews:</b>\
+			<ul>\
+			<li> http://www.addictivetips.com/web/fruumo-download-manager/</li>\
+			<li> http://dottech.org/137728/chrome-review-fruumo-download-manager-extension/</li>\
+			</ul>'
+	    },
+	    fruumotm: {
+	        title: 'Fruumo Tab Manager',
+	        screenshot: '/static/images/project_screenshots/fruumotm.jpg',
+	        desc: 'Fruumo Tab Manager was designed for people who have tons of tabs open all the time, and provides features that lets the users manipulate their tabs from a central UI.Search, close, pin, move to new window etc. <br/> <br/>\
+			<b> Achievements: </b>\
+			<ul>\
+			<li>Has a 5 star rating on the Chrome Webstore</li>\
+			</ul>\
+			<b>Reviews:</b>\
+			<ul>\
+			<li> http://www.lostintechnology.com/windows/manage-google-chrome-browser-tabs-fruumo-tab-manager/</li>\
+			</ul>'
+	    },
+	    hyt: {
+	        title: 'HearYoutube',
+	        screenshot: '/static/images/project_screenshots/hyt.jpg',
+	        desc: 'A website which lets users convert their favourite Youtube Videos to mp3 format. Came with it’s own chrome extension that’d let the users convert it right on youtube.com, and it’s android app that would let them do it right from within the Youtube Android App. <br/> <br/>\
+			<b> Why it was shut down: </b> <br/>\
+			Google banned my adsense account as i was displaying ads on websites that violated their T & C. They then went on to block all of HearYoutubes server IP Addresses, thereby preventing any further downloading.<br/><br/>\
+			<b> Achievements: </b>\
+			<ul>\
+			<li>Has a 5 star rating on the Chrome Webstore</li>\
+			</ul>\
+			<b>Reviews:</b>\
+			<ul>\
+			<li> http://www.baixaki.com.br/download/hearyoutube.htm</li>\
+			</ul>'
+	    },
+	    stdpd: {
+	        title: 'Studenclopedia',
+	        screenshot: '/static/images/projects/stdpd.png',
+	        desc: 'This was a project i did for my tuition center, that would allow the management of students efficiently, and allow the Students and their parents to contact their teachers with ease. It had a complete graphing system to graph the student’s performance over time in their tests, a receipting system that’d allow the center to issue receipts to the parents, and a messaging system with attachments.'
+	    },
+	    ca: {
+	        title: 'Chrome Assistant',
+	        screenshot: '/static/images/project_screenshots/ca.jpg',
+	        desc: 'It was released around the time Siri was announced for iPhones, and it was designed to mimic the “Siri Experience” on the browser. It’d give the users a voice assistant activated by a hotkey within chrome, allowing them to look for locations, translate text, look up words meanings, search for websites, and essentially control their browsers with their voices. <br/> <br/>\
+			<b> Why it was shut down: </b> <br/>\
+			It became insanely popular during it’s peak, but as voice assistants became more standard in phones, the hype died down, and it was no longer worth running this service, hence i shut it down.<br/><br/>\
+			<b> Achievements: </b>\
+			<ul>\
+			<li>During it’s life it hit 190,098,149 pageviews</li>\
+			<li>370,000+ returning users and each user spending an average of 20 minutes on the service.</li>\
+			<li>During it’s peak it was hitting 3000 Active users at any one moment, and 1000s of commands being processed every second.</li>\
+			</ul>\
+			<b>Reviews:</b>\
+			<ul>\
+			<li> http://www.addictivetips.com/internet-tips/chrome-assistant-converts-speech-to-executable-browsing-commands/</li>\
+			</ul>'
+	    },
+	    thunderdj: {
+	        title: 'Thunder.dj',
+	        screenshot: '/static/images/project_screenshots/thunderdj.jpg',
+	        desc: 'This project was thrown together over a weekend, as a challenge to myself. It is designed to help users in a small party add to a youtube playlist from their phones, while the party host’s computer was connected to speakers. It would let users upvote and downvote the youtube videos, and push songs up or down in the list, all this is being done live, with live updates across everyone’s phones and on the nice UI Displayed on the PC.'
+	    },
+	    jeans: {
+	        title: 'Jeans for Pocket',
+	        screenshot: '/static/images/project_screenshots/jeans.jpg',
+	        desc: 'Jeans was the first full-fledged Pocket(formerly read-it-later) Client for windows 8.'
+	    },
+	    quip: {
+	        title: '7Quip',
+	        screenshot: '/static/images/project_screenshots/7q.jpg',
+	        desc: 'It was essentially a 9Gag for the super popular 7 second Vine Videos. It would let users submit, up-vote and down-vote Vine videos, complete with a trending, New and Hot Pages, using it’s own algorithms. <br/> <br/>\
+			<b> Why it was shut down: </b> <br/>\
+			It was shut-down as before starting the project i thought i’d be able to make up for a lack of community by writing my own robot to submit vine videos in the beginning, but i was sadly wrong, the website never really gained the mass popularity it required to succeed and was shut down; even with the robot to auto curate the funniest vines from Vine.<br/><br/>\
+			<b> Achievements: </b><br/>\
+			This website was the first service i wrote that was designed with a massive amount of users in mind. It was written with it’s own load balancer that i wrote, this load balancer was able to spin up new servers when the load was too high, and kill servers when the load didn’t call extra servers.'
+	    },
+	    fact: {
+	        title: 'FACT Programming',
+	        screenshot: '/static/images/projects/fact.png',
+	        desc: 'It was basically a set of Java Classes written to make learning java a lot more approachable, by simplifying alot of the basic functions even further. I wrote an IDE to accompany FACT Engine and make it even easier as it’d add the necessary code to allow you to write JAVA as if it were a scripting language. I ported and simplified it over for it to run on Jailbroken iOS Devices on the JVM written by Saurik.'
+	    },
+	    noq: {
+	        title: 'NoQ',
+	        screenshot: '/static/images/projects/noq.png',
+	        desc: 'NoQ was written at another NUSACM Hackathon, it didn’t win but was included in the article about the hackathon on a famous tech blog. NoQ was designed as a way to beat queues at restaurants, doctors, hospitals etc. It would allow the users to “get in queue” by scanning a QR Code, and they would receive a notification once they were in front of the queue. <br/> <br/>\
+			<b> Review: </b><br/>\
+			http://e27.co/stage/blog/developer-weekend-a-success-participants-learned-valuable-windows-azure-skills/'
+	    },
+	    scat: {
+	        title: 'Scat',
+	        screenshot: '/static/images/project_screenshots/scat.png',
+	        desc: 'Scat was one of the first few Youtube Clients for Windows Phone, while Google was refusing to release a Youtube Client for Windows'
+	    },
+	    blick: {
+	        title: 'Security Debugger @ Blick.sg',
+	        screenshot: '/static/images/projects/blick.png',
+	        desc: 'I started working for Blick.sg after i emailed them about multiple exploitable flaws in their Payment systems, which allowed me to recieve items worth thousands of dollars for 1 cent. I found other exploits which would allow me to bid for items and win, without my name showing up on the item to the public, and exploits that were able to stop and crash bids while they were in progress.'
+	    },
+	    livewell: {
+	        title: 'Freelancer @ LiveWell Collaborative',
+	        screenshot: '/static/images/projects/livewell.png',
+	        desc: 'At Live Well Collaborative i built an android launcher for iNOMobile. They were building a tablet aimed at the older generations, hence they needed a launcher to make the experience easier for the older generations, it turned out to be an amazing project with all the software finalized. Sadly the product never made it to the market.'
+	    },
+	    sunlight: {
+	        title: 'Freelancer @ Sunlight Sports',
+	        screenshot: '/static/images/projects/sunlight.jpg',
+	        desc: 'During my time at Sunlight Sports I wrote an Android Application that was intended to be run on android tablets at all Li-Ning stores across India as a Product Assistant. This application would ask the user to answer a quick questionnaire and it would recommend the best badminton rackets based off of this questionnaire.'
+	    }
+
+
+	};
